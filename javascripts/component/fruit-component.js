040edfa76d966318b65b1a7c74300a1bbca24afe
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2022-10-17 21:13:58 +0800

    Possible bug fix to offset bug

--- javascripts/component/fruit-component.js
+++ javascripts/component/fruit-component.js
@@ -4,11 +4,6 @@ Vue.component("fruit-component", {
         selectBoxPos: Object,
         getState: Boolean
     },
-    data() {
-        return {
-            avgPos: [0, 0]
-        }
-    },
     computed: {
         selected() {
             if (this.fruit.isDeleted()) return false;
@@ -17,11 +12,16 @@ Vue.component("fruit-component", {
             if (start[0] == end[0] && start[1] == end[1]) {
                 return false
             }
+
+            
+            const rect = this.$el.getBoundingClientRect();
+            const [posX, posY] = [(rect.left + rect.right) / 2, (rect.top + rect.bottom) / 2]
+
+            const hitboxOffset = 2;
             const [scrollX, scrollY] = [window.scrollX, window.scrollY]
             const [minX, minY] = [Math.min(start[0], end[0]) + scrollX, Math.min(start[1], end[1]) + scrollY]
             const [maxX, maxY] = [Math.max(start[0], end[0]) + scrollX, Math.max(start[1], end[1]) + scrollY]
-            const [posX, posY] = this.avgPos;
-            return minX <= posX && posX <= maxX && minY <= posY && posY <= maxY;
+            return minX - hitboxOffset <= posX && posX <= maxX + hitboxOffset && minY - hitboxOffset <= posY && posY <= maxY + hitboxOffset;
         },
         path() {
             if (this.selected) {
@@ -36,8 +36,6 @@ Vue.component("fruit-component", {
         }
     },
     mounted() {
-        const rect = this.$el.getBoundingClientRect();
-        this.avgPos = [(rect.left + rect.right) / 2, (rect.top + rect.bottom) / 2]
     },
     template:
     `<div class="component fruit-component disable-select" :class="{'hidden': fruit.isDeleted()}">
